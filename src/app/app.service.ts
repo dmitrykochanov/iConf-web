29683e7c1a942a52d478bcd4da9ba33a05061245
Dmitry <dmitrykochanov1@gmail.com>
2018-12-24 17:11:55 +0400

    readme fix

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,12 +1,12 @@
-import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Conference} from './entities/Conference';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
-import {SignUpRequest} from './entities/SignUpRequest';
+import { SignUpRequest } from './entities/SignUpRequest';
 
-import {BaseResponse} from './entities/BaseResponse';
-import {AuthResponse} from './entities/AuthResponse';
-import {SignInRequest} from './entities/SignInRequest';
+import { BaseResponse } from './entities/BaseResponse';
+import { AuthResponse } from './entities/AuthResponse';
+import { SignInRequest } from './entities/SignInRequest';
+import { Conference } from './entities/Conference';
 
 @Injectable({
   providedIn: 'root'
@@ -17,26 +17,43 @@ export class ConferencesService {
   constructor(private client: HttpClient) {
   }
 
-  getConferences() {
-    return this.client.get<BaseResponse<Conference[]>>(this.BASE_URL + '/conferences');
-  }
-
-  getConferencesById(conferenceId: number) {
-    return this.client.get<BaseResponse<Conference>>(this.BASE_URL + '/conference/' + conferenceId);
-  }
-
-  addParticipantToConference(conferenceId: string, userId: string) {
-    return this.client.post<BaseResponse<AuthResponse>>(this.BASE_URL + '/conferences/' + conferenceId + '/participants', {
-      id: userId,
-      email: 'test'
-    });
-  }
-
   signIn(signInRequest: SignInRequest) {
     return this.client.post<BaseResponse<AuthResponse>>(this.BASE_URL + '/users/sign-in', signInRequest);
   }
 
   signUp(signUpRequest: SignUpRequest) {
     return this.client.post<BaseResponse<AuthResponse>>(this.BASE_URL + '/users/sign-up', signUpRequest);
+  }
+
+  getConferences() {
+    return this.client.get<BaseResponse<Conference[]>>(this.BASE_URL + '/conferences');
+  }
+
+  getConference(conferenceId: number) {
+    return this.client.get<BaseResponse<Conference>>(this.BASE_URL + '/conferences/' + conferenceId);
+  }
+
+  createConference(conference: Conference) {
+    return this.client.post<BaseResponse<null>>(this.BASE_URL + '/conferences', conference);
+  }
+
+  deleteConference(conferenceId: number) {
+    return this.client.delete<BaseResponse<null>>(this.BASE_URL + '/conferences/' + conferenceId);
+  }
+
+  addParticipantToConference(conferenceId: number, participantId: number) {
+    return this.client.post<BaseResponse<null>>(this.BASE_URL + '/conferences/' + conferenceId + '/participants/' + participantId, null);
+  }
+
+  removeParticipantFromConference(conferenceId: number, participantId: number) {
+    return this.client.delete<BaseResponse<null>>(this.BASE_URL + '/conferences/' + conferenceId + '/participants/' + participantId);
+  }
+
+  participateInConference(conferenceId: number, participate: boolean) {
+    if (participate) {
+      return this.client.post<BaseResponse<null>>(this.BASE_URL + '/conferences/' + conferenceId + '/participate', null);
+    } else {
+      return this.client.delete<BaseResponse<null>>(this.BASE_URL + '/conferences/' + conferenceId + '/participate');
+    }
   }
 }
